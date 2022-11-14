afe91a958b2dee3557345345870a44cffb3882d9
waterself98 <kimychsw@gmail.com>
2022-11-14 22:23:03 +0900

    fixed: MergeSort.h

---
+++ sort/MergeSort/MergeSort.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef MERGESORT_H
+
+void arraySwap(int* in, int a, int b); //swap a and b in array in
+
+bool SortCheck(int*array, int size); //ascending sort check
+
+int* MakeRandomArray(int size);
+
+#endif // !MERGESORT_H
